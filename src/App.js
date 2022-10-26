b21578543c4d3620374936dc18fd592ab343603f
Aprameya Shankar <aprameyashankar@gmail.com>
2022-10-26 21:27:11 +0530

    Added all files

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import './app.scss'
+import Beginner from './components/beginner';
+import Intermediate from './components/intermediate';
+import Master from './components/master';
+
+function App() {
+  	const [difficulty, setDifficulty] = useState(() => {
+		return "Beginner";
+	});
+	var order=9;
+	var numberOfBombs=9;
+	function setOrder(difficulty){
+		if(difficulty == "Beginner"){order = 9;}
+		else if(difficulty == "Intermediate"){order = 16;}
+		else if(difficulty == "Master"){order = 22;}
+		return;
+	}
+	function setBombCount(difficulty){
+		if(difficulty == "Beginner"){numberOfBombs=10;}
+		else if(difficulty == "Intermediate"){numberOfBombs=19;}
+		else if(difficulty == "Master"){numberOfBombs=26;}
+		return;
+	}
+	setOrder(difficulty);
+	setBombCount(difficulty);
+
+	function refresh(){
+        window.location.reload();
+		return;
+    }
+
+	return (
+		<>
+		<main>
+			<menu>
+				<h1>Minesweeper</h1>
+				<span>Choose Difficulty:</span>
+				<Link to="/beginner"><button className='beginner-btn' onClick={() => setDifficulty("Beginner")}>Beginner</button></Link>
+				<Link to="/intermediate"><button className='intermediate-btn' onClick={() => setDifficulty("Intermediate")}>Intermediate</button></Link>
+				<Link to="/master"><button className='master-btn' onClick={() => setDifficulty("Master")}>Master</button></Link>
+				<div className='details'>
+					<h3>Difficulty:<br/><b>{difficulty}</b></h3>
+					<h3>Grid:<br/><b>{order} x {order}</b></h3>
+					<h3>Number Of Bombs<br/><b>{numberOfBombs}</b></h3>
+					<h3>made by aprameya shankar</h3>
+				</div>
+				<a  href="javascript:location.reload(true)"><button className='restart'>Start Over</button></a>
+
+			</menu>
+			<div className='board'>
+				<Routes>
+					<Route path='/beginner' element={<Beginner />} />
+					<Route path='/intermediate' element={<Intermediate />} />
+					<Route path='/master' element={<Master />} />
+				</Routes>
+			</div>
+		</main>
+		<footer>© 2022 All rights reserved.</footer>
+		</>
+	)
+}
+
+
+export default App;
---
+++ src/components/intermediate.jsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import { useEffect } from 'react';
+import './intermediate.scss'
+
+
+function Intermediate() {
+    const newdifficulty = 16;
+    const grid = new Array(newdifficulty).fill(0).map(row => new Array(newdifficulty).fill(0))
+    useEffect(() => {
+        const grid = document.querySelector('.grid');
+        grid.style.gridTemplateColumns = `repeat(${newdifficulty}, 1fr)`
+        
+    })
+    const n=newdifficulty;
+    const bombCount = 19;
+    //Place bombs
+    for(let i=0; i<bombCount; i++){ 
+        let random1 = Math.floor(Math.random() * n);
+        let random2 = Math.floor(Math.random() * n);
+        if(grid[random1][random2] === '💣'){
+            i--;
+            continue;
+        }
+        else{
+            grid[random1][random2] = '💣';
+        }
+    }  
+    //Place count cells
+    for(let i=0; i<n; i++){
+        for(let j=0; j<n; j++){
+            if(grid[i][j]==='💣'){
+                validCountBlock(i-1, j, n);
+                validCountBlock(i-1, j+1, n);
+                validCountBlock(i, j+1, n);
+                validCountBlock(i+1, j+1, n);
+                validCountBlock(i+1, j, n);
+                validCountBlock(i+1, j-1, n);
+                validCountBlock(i, j-1, n);
+                validCountBlock(i-1, j-1, n);
+            }
+        }
+    }
+    function validCountBlock(p, q, n){ //Assigns count only in the cell is valid
+        if(p>=0 && p<n && q>=0 && q<n && grid[p][q]!=='💣')
+		grid[p][q]+=1;
+        return;
+    }
+    const baseButtonColor = "#d0d0d7";
+    const flagColor = "rgb(219, 1, 31)";
+    useEffect(() => {
+        for(let i=0; i<n; i++){
+            for(let j=0; j<n; j++){
+                if(grid[i][j]=='💣'){
+                    document.querySelector(`.cell-${i}-${j} .cell-text`).style.display = "none";
+                }
+                var cellButton = document.querySelector(`.cell-${i}-${j} .cell-btn`);
+                cellButton.addEventListener("contextmenu", function(e){
+                    e.preventDefault();
+                    if(document.querySelector(`.cell-${i}-${j} .cell-btn`).style.backgroundColor == flagColor){
+                        document.querySelector(`.cell-${i}-${j} .cell-btn`).style.backgroundColor = "rgb(0, 0, 0)";
+                        document.querySelector(`.cell-${i}-${j} .cell-btn`).addEventListener("mouseover", function(){
+                            document.querySelector(`.cell-${i}-${j} .cell-btn`).style.backgroundColor = baseButtonColor;
+                        })
+                        document.querySelector(`.cell-${i}-${j} .cell-btn`).addEventListener("mouseout", function(){
+                            document.querySelector(`.cell-${i}-${j} .cell-btn`).style.backgroundColor = "rgb(0, 0, 0)";
+                        })
+                    }
+                    else {
+                        console.log("righty");
+                        document.querySelector(`.cell-${i}-${j} .cell-btn`).style.backgroundColor = flagColor;
+                    }
+                });
+                cellButton.addEventListener("click", function(){
+                    if(document.querySelector(`.cell-${i}-${j} .cell-btn`).style.backgroundColor != flagColor){
+                        ifBomb(i, j);
+                        //console.log("clickety");
+                        document.querySelector(`.cell-${i}-${j} .cell-btn`).style.display = "none";    
+                        
+                        document.querySelector(`.cell-${i}-${j} .cell-text`).innerText = grid[i][j];
+                    }
+                });
+                
+            }
+        }
+    })
+
+    function ifBomb(i, j){
+        if(grid[i][j] === '💣'){
+            const allCellButtons = document.querySelectorAll('.cell-btn');
+            allCellButtons.forEach((cellButton) => {
+                cellButton.style.display = "none";
+                document.querySelector('.grid').style.pointerEvents = "none";
+                document.querySelector('.game-over').style.display = "none";
+                document.querySelector(`.cell-${i}-${j} .cell-text`).style.display = "none";
+
+            });
+            for(let i=0; i<n; i++){
+                for(let j=0; j<n; j++){
+                    document.querySelector(`.cell-${i}-${j} .cell-text`).innerText = grid[i][j];
+                }
+            }
+        }
+        return;
+    }
+
+    return (
+        <>
+            <div className='grid'>
+                {grid.map((row, rowId) => (
+                    <div key={rowId} className='row'>
+                        {row.map((cell, cellId) => (
+                            <div key={cellId} className={`cell cell-${rowId}-${cellId}`}>
+                                <div className='cell-text'></div>
+                                <button className='cell-btn'></button>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <div className='game-over'><span>Game Over</span></div>
+        </>
+    )
+}
+
+export default Intermediate;
